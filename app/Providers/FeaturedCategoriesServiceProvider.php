6c040ce817880555a9b2d2f782673688792c9084
suraiyaaysha <suraiyaaysha.cse@gmail.com>
2023-05-16 14:40:28 +0600

    feat: Implement  view composer for featured categories
    -  Remove commented code from view composer to share featured categories with views

--- app/Providers/FeaturedCategoriesServiceProvider.php
+++ app/Providers/FeaturedCategoriesServiceProvider.php
@@ -23,11 +23,7 @@ class FeaturedCategoriesServiceProvider extends ServiceProvider
     {
         // Retrieve featured categories here and share them with views
         $featuredCategories = Category::where('is_featured', true)->take(4)->get();
-        // return view('frontend.home', ['categories' => $featuredCategories, 'featuredCategories' => $featuredCategories]);
 
-
-        // view()->composer('layouts.app', function ($view) use ($featuredCategories) {
-        //     $view->with('featuredCategories', $featuredCategories);
         view()->composer('frontend.layouts.app', function ($view) use ($featuredCategories) {
             $view->with(['categories' => $featuredCategories, 'featuredCategories' => $featuredCategories]);
         });
